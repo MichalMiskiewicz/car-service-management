6b94c660372f66b951389c5dc67e8046c2b1904a
MichalMiskiewicz <miskiewiczmd@gmail.com>
2022-10-21 12:23:08 +0200

    Add entities and relations between them

---
+++ src/main/java/com/miskiewicz/michal/carservicemanagement/Entity/User.java
@@ -0,0 +1,43 @@
+package com.miskiewicz.michal.carservicemanagement.Entity;
+
+import com.miskiewicz.michal.carservicemanagement.Entity.Enums.UserType;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.Type;
+
+import javax.persistence.*;
+import java.util.List;
+import java.util.UUID;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(generator = "uuid2")
+    @GenericGenerator(name = "uuid2", strategy = "uuid2")
+    @Column(name = "id", updatable = false, nullable = false, columnDefinition = "VARCHAR(36)")
+    @Type(type = "uuid-char")
+    private UUID id;
+
+    private String firstname;
+
+    private String lastname;
+
+    private String email;
+
+    @Enumerated(EnumType.STRING)
+    private UserType userType;
+
+    @ManyToOne
+    private Address address;
+
+    @OneToMany
+    private List<Car> car;
+}
