b441633e1b80d41f0e74a91864f657bf93c32863
MichalMiskiewicz <miskiewiczmd@gmail.com>
2022-10-24 12:43:16 +0200

    refactor

--- src/main/java/com/miskiewicz/michal/carservicemanagement/Repository/AddressRepository.java
+++
@@ -1,9 +0,0 @@
-package com.miskiewicz.michal.carservicemanagement.Repository;
-
-import com.miskiewicz.michal.carservicemanagement.Entity.Address;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.UUID;
-
-public interface AddressRepository extends JpaRepository<Address, UUID> {
-}
--- src/main/java/com/miskiewicz/michal/carservicemanagement/Repository/CarRepository.java
+++
@@ -1,9 +0,0 @@
-package com.miskiewicz.michal.carservicemanagement.Repository;
-
-import com.miskiewicz.michal.carservicemanagement.Entity.Car;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.UUID;
-
-public interface CarRepository extends JpaRepository<Car, UUID> {
-}
